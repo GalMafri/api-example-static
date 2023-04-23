9d1e7d0741851c5e613508b0616dfa0150536fea
galMaf <gal@layer.co.il>
2023-04-23 14:15:55 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
+
+function HomePage({ products }) {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: "/no-data",
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }; // open 404 page
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10, // build the page after 10 seceonds
+  };
+}
+
+export default HomePage;
